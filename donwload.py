dd2011469dc962b471ee7019aac645abd6d39e25
GabrielBentoG <gabrielbentog0@gmail.com>
2023-06-14 15:48:42 -0300

    small changes

--- donwload.py
+++ donwload.py
@@ -1,6 +1,8 @@
 import gdown
 
-folder_id = 'FOLDER_ID'
+# Donwload a complete folder
+folder_id = ''
 #url = f'https://drive.google.com/drive/folders/{folder_id}?usp=sharing'
 #gdown.download_folder(url, quiet=True, use_cookies=False)
 gdown.download_folder(id=folder_id, quiet=False, use_cookies=False)
+
